93cdafe49a231391343c4482cf48f1f706657ee7
Anis Zaatour <anis@prima-it.consulting>
2022-10-09 14:54:06 +0100

    aggregate

--- src/controller/postController.ts
+++ src/controller/postController.ts
@@ -1,6 +1,7 @@
 import { request, Request, response, Response } from "express";
+import mongoose from "mongoose";
 import postModel from "../model/postModel";
-
+import Post from "../model/postModel";
 
 
 
@@ -13,23 +14,34 @@ import postModel from "../model/postModel";
  *
  * @returns {Response} endpoint response
  */
-const  getPosts = async (req: Request, res: Response) => {
-    const postByPostName = await postModel.aggregate(
-        [
-          {
-            $match: {
-              _id: {
-                postName: '$postModel.postName',
-              },
-            },
-          },
-        ]
-      );
-      response.send({
-        postByPostName
-      });
-    }
-   
-  
+ const getPosts =async (req: Request, res: Response) => {
+    const postId=req.body.params
+    const post =await postModel.aggregate([
+        {$match:{_id:new mongoose.Types.ObjectId(postId)},}
+    ])
+    return Post.find()
+      .then((posts) =>
+        res.status(200).json(
+          posts.map((post) => ({
+            postName: post.postName,
+            description: post.description,
+            numberOfLikes: post.numberOfLikes,
+            dateOfPost: post.dateOfPost,
+          }))
+        )
+      )
+      .catch((error) => res.status(500).json({ error }));
+  };
 
 export default {getPosts};
+// const id = JSON.parse(req.params.postId);
+//   const post = await postModel.aggregate(
+//     [{ $match: { _id: new mongoose.Types.ObjectId(id) } }],
+//     (err, post) => {
+//       if (err) {
+//         console.log(err.message);
+//         return res.send(`Post id: ${req.params.postId} not found!`);
+//       }
+//       res.status(200).send(post[0]);
+//     }
+//   );
--- src/routes/postRoute.ts
+++ src/routes/postRoute.ts
@@ -1,11 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 import controller from "../controller/postController";
+import postModel from "../model/postModel";
 
 const router = express.Router();
 
 
-router.get("/list", controller.getPosts);
-
-
+router.get(`/list:postId`, controller.getPosts);
 export = router;
 
